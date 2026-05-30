7966ca919278393a1039b8eefc94ee9a7d67c0a5
Mohamed AIT ALI <moha4med.bns@gmail.com>
2026-05-30 19:14:34 +0100

    fix: correct image content type in stream response and clean up camera import

--- app/services/stream_service.py
+++ app/services/stream_service.py
@@ -1,18 +1,16 @@
 # app/services/stream_service.py
 
 import cv2
-from app.sensors.camera import Camera
-
-camera = Camera()
+from app.sensors.camera import camera
 
 def generate_stream():
     while True:
         frame = camera.get_frame()
 
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
         _, buffer = cv2.imencode('.jpg', frame)
         frame_bytes = buffer.tobytes()
 
         yield (b'--frame\r\n'
-               b'Content-Type: iamge/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
