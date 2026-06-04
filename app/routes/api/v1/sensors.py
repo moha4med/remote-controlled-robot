2d5d567e72e6e4970867ae7e0adfa295d92c940c
Mohamed AIT ALI <moha4med.bns@gmail.com>
2026-06-04 20:48:21 +0100

    fix: fix sensor route naming

--- app/routes/api/v1/sensors.py
+++ app/routes/api/v1/sensors.py
@@ -5,7 +5,7 @@ sensors_bp = Blueprint("sensors", __name__, url_prefix="/api/v1/sensors")
 
 sensor = USBTempHumiditySensor("/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0")
 
-@sensors_bp.route("/sensors", methods=["GET"])
+@sensors_bp.route("/", methods=["GET"])
 def environment():
     data = sensor.read()
     return jsonify(data)
