6e613605bb5694e4dbd2664f83ade67351733565
Mohamed AIT ALI <moha4med.bns@gmail.com>
2026-06-04 22:40:24 +0100

    fix: remove deprecated eventlet package

--- app/extensions.py
+++ app/extensions.py
@@ -5,6 +5,11 @@ from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_socketio import SocketIO
 
+socketio = SocketIO(
+    cors_allowed_origins="*",
+    async_mode="threading"
+)
+
 db = SQLAlchemy()
 jwt = JWTManager()
 bcrypt = Bcrypt()
