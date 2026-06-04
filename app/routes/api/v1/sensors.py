f831801254fc2e967394ea830d6bbee994558db3
Mohamed AIT ALI <moha4med.bns@gmail.com>
2026-06-04 20:40:53 +0100

    fix: change usb sensor name as rpi detect it

--- app/routes/api/v1/sensors.py
+++ app/routes/api/v1/sensors.py
@@ -3,7 +3,7 @@ from app.sensors.usb_temp_humidity import USBTempHumiditySensor
 
 sensors_bp = Blueprint("sensors", __name__, url_prefix="/api/v1/sensors")
 
-sensor = USBTempHumiditySensor("COM5")
+sensor = USBTempHumiditySensor("/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0")
 
 @sensors_bp.route("/sensors", methods=["GET"])
 def environment():
